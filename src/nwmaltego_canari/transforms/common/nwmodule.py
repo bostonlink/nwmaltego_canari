3b7ca420804b46502fe7b9d39d66a2dda63392b8
bostonlink <bostonlink@pentest-labs.org>
2013-01-22 10:51:09 -0500

    added username and password easygui popup to replace username and pass stored in config file

--- src/nwmaltego_canari/transforms/common/nwmodule.py
+++ src/nwmaltego_canari/transforms/common/nwmodule.py
@@ -6,24 +6,47 @@
 # Author: David Bressler
 # Netwitness python functions to interface with the NW REST API
 
-import urllib2, urllib, sys
-from canari.config import config
+import urllib2, urllib
 
-# HTTP Basic Authentication to NW REST API
+from os import path
+from canari.config import config
+from canari.easygui import multpasswordbox
+from canari.utils.fs import cookie, fsemaphore
 from canari.maltego.message import MaltegoException
 
+def get_creds():
+	fn = cookie('netwitness')
+	if not path.exists(fn):
+		f = fsemaphore(fn, 'wb')
+		f.lockex()
+		msg = 'Please enter your Netwitness credentials'
+		fv = multpasswordbox(msg, 'Netwitness Credentials', ['Username:', 'Password:'])
+		nwu, nwp = fv
+		f.write('username=%s#password=%s' % (nwu, nwp))
+	else:
+		f = fsemaphore(fn)
+		f.locksh()
+		creds = f.read().split('#')
+		for i in creds:
+			if 'username' in i:
+				parse = i.split('=')
+				nwu = parse[1]
+			if 'password' in i:
+				parse = i.split('=')
+				nwp = parse[1]
+	return nwu, nwp
+
 def nw_http_auth():
+	"""Authenticates to the NW REST API via HTTP Basic authentication"""
+	nwu, nwp = get_creds()
+	auth_handler = urllib2.HTTPBasicAuthHandler()
+	auth_handler.add_password(realm = 'NetWitness',
+							  uri = config['netwitness/nw_concentrator'],
+							  user = nwu,
+							  passwd = nwp )
 
-    """Authenticates to the NW REST API via HTTP Basic authentication"""
-
-    auth_handler = urllib2.HTTPBasicAuthHandler()
-    auth_handler.add_password(realm = 'NetWitness',
-                              uri = config['netwitness/nw_concentrator'],
-                              user = config['netwitness/username'],
-                              passwd = config['netwitness/password'] )
-
-    opener = urllib2.build_opener(auth_handler)
-    urllib2.install_opener(opener)
+	opener = urllib2.build_opener(auth_handler)
+	urllib2.install_opener(opener)
 
 # Function builds full URL for NW REST API Query and returns the results
 #
@@ -35,71 +58,71 @@ def nw_http_auth():
 
 
 def get_http_data(full_url):
-    try:
-        req = urllib2.Request(full_url)
-        ret = urllib2.urlopen(req)
-        ret_data = ret.read()
-        return ret_data
-    except urllib2.HTTPError as e:
-        raise MaltegoException("The Transform has returned: %s" % e)
+	try:
+		req = urllib2.Request(full_url)
+		ret = urllib2.urlopen(req)
+		ret_data = ret.read()
+		return ret_data
+	except urllib2.HTTPError as e:
+		raise MaltegoException("The Transform has returned: %s" % e)
 
 
 def nwQuery(id1, id2, query_string, cType, size):
 
-    """ Queries the NW REST API and returns the results 
-    Example query that would be passed to the function in the query_string variable:
-    query = 'select service,ip.src,country.dst where service=80'"""
+	""" Queries the NW REST API and returns the results 
+	Example query that would be passed to the function in the query_string variable:
+	query = 'select service,ip.src,country.dst where service=80'"""
 
-    nwa = config['netwitness/nw_concentrator']
+	nwa = config['netwitness/nw_concentrator']
 
-    base_uri = "/sdk?msg=query&"
-    params_dic = {'force-content-type': cType, 'expiry': 600, 'id1': id1, 'id2': id2, 'size': size,
-                  'query': query_string}
+	base_uri = "/sdk?msg=query&"
+	params_dic = {'force-content-type': cType, 'expiry': 600, 'id1': id1, 'id2': id2, 'size': size,
+				  'query': query_string}
 
-    enc_params = urllib.urlencode(params_dic)
-    full_url = nwa + base_uri + enc_params
+	enc_params = urllib.urlencode(params_dic)
+	full_url = nwa + base_uri + enc_params
 
-    return get_http_data(full_url)
+	return get_http_data(full_url)
 
 #  Retrieves the meta id range for the session range
 
 def nwSession(id1, id2, cType):
 
-    """ Returns the meta id for a specific session range.  
-    If id1=0 and id2=0 it returns the meta id range for all data """
+	""" Returns the meta id for a specific session range.  
+	If id1=0 and id2=0 it returns the meta id range for all data """
 
-    nwa = config['netwitness/nw_concentrator']
+	nwa = config['netwitness/nw_concentrator']
 
-    base_uri = "/sdk?msg=session&"
-    params_dic = {'force-content-type': cType, 'expiry': 600, 'id1': id1, 'id2': id2}
+	base_uri = "/sdk?msg=session&"
+	params_dic = {'force-content-type': cType, 'expiry': 600, 'id1': id1, 'id2': id2}
 
-    enc_params = urllib.urlencode(params_dic)
-    full_url = nwa + base_uri + enc_params
+	enc_params = urllib.urlencode(params_dic)
+	full_url = nwa + base_uri + enc_params
 
-    return get_http_data(full_url)
+	return get_http_data(full_url)
 
 # values: Performs a query and returns the matching values for a report
 # example: nwValue(nwa, 0, 0, 100, 'risk.warning', 'text/plain')
 
 def nwValue(id1, id2, size, fieldname, cType, where=''):
 
-    """ Returns a values associated with a meta type.
-    If the where_clause is used, you can return specific values of a certain type.
-    For example:
+	""" Returns a values associated with a meta type.
+	If the where_clause is used, you can return specific values of a certain type.
+	For example:
 
-    nwmodule.nwValue(nwa, 0, 0, 100, 'risk.warning', 'text/plain')
+	nwmodule.nwValue(nwa, 0, 0, 100, 'risk.warning', 'text/plain')
 
-    returns all values associated with the risk.warning meta type."""
+	returns all values associated with the risk.warning meta type."""
 
-    nwa = config['netwitness/nw_concentrator']
-    base_uri = "/sdk?msg=values&"
-    params_dic = {'force-content-type': cType, 'expiry': 600, 'id1': id1, 'id2': id2, 'size': size,
-                  'fieldName': fieldname, 'where': where}
+	nwa = config['netwitness/nw_concentrator']
+	base_uri = "/sdk?msg=values&"
+	params_dic = {'force-content-type': cType, 'expiry': 600, 'id1': id1, 'id2': id2, 'size': size,
+				  'fieldName': fieldname, 'where': where}
 
-    enc_params = urllib.urlencode(params_dic)
-    full_url = nwa + base_uri + enc_params
+	enc_params = urllib.urlencode(params_dic)
+	full_url = nwa + base_uri + enc_params
 
-    return get_http_data(full_url)
+	return get_http_data(full_url)
 
 
 # timeline: Returns the count of sessions/size/packets in discrete time intervals
@@ -107,30 +130,30 @@ def nwValue(id1, id2, size, fieldname, cType, where=''):
 
 def nwTimeline(time1, time2, size, cType, where=''):
 
-    """ Returns the count of sessions/size/packets in discrete time intervals """
+	""" Returns the count of sessions/size/packets in discrete time intervals """
 
-    nwa = config['netwitness/nw_concentrator']
-    base_uri = "/sdk?msg=timeline&"
-    params_dic = {'force-content-type': cType, 'expiry': 600, 'time1': time1, 'time2': time2, 'size': size,
-                  'where': where}
+	nwa = config['netwitness/nw_concentrator']
+	base_uri = "/sdk?msg=timeline&"
+	params_dic = {'force-content-type': cType, 'expiry': 600, 'time1': time1, 'time2': time2, 'size': size,
+				  'where': where}
 
-    enc_params = urllib.urlencode(params_dic)
-    full_url = nwa + base_uri + enc_params
+	enc_params = urllib.urlencode(params_dic)
+	full_url = nwa + base_uri + enc_params
 
-    return get_http_data(full_url)
+	return get_http_data(full_url)
 
 
 # Returns all queryable fields and definitions wtihin NW
 
 def nwLanguage(cType):
 
-    """ Returns all field types and definitions you can query wtihin NW """
+	""" Returns all field types and definitions you can query wtihin NW """
 
-    nwa = config['netwitness/nw_concentrator']
-    base_uri = "/sdk?msg=language&"
-    params_dic = {'force-content-type': cType, 'expiry': 600, 'size': 200}
+	nwa = config['netwitness/nw_concentrator']
+	base_uri = "/sdk?msg=language&"
+	params_dic = {'force-content-type': cType, 'expiry': 600, 'size': 200}
 
-    enc_params = urllib.urlencode(params_dic)
-    full_url = nwa + base_uri + enc_params
+	enc_params = urllib.urlencode(params_dic)
+	full_url = nwa + base_uri + enc_params
 
-    return get_http_data(full_url)
+	return get_http_data(full_url)
